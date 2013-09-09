be30b85e209a692e0e88157c2f8536feca5ce5c7
andri <andriniaina@gmail.com>
2013-09-09 22:19:08 +0200

    hopefully a final grammar

--- Samples_syntax3.fsx
+++ Samples_syntax3.fsx
@@ -1,7 +1,7 @@
 ﻿
 
-#r @"packages\FSPowerPack.Core.Community.3.0.0.0\Lib\Net40\FSharp.PowerPack.dll"
-#r @"packages\FSPowerPack.Linq.Community.3.0.0.0\Lib\Net40\FSharp.PowerPack.Linq.dll"
+//#r @"packages\FSPowerPack.Core.Community.3.0.0.0\Lib\Net40\FSharp.PowerPack.dll"
+//#r @"packages\FSPowerPack.Linq.Community.3.0.0.0\Lib\Net40\FSharp.PowerPack.Linq.dll"
 
 #r @"packages\RhinoMocks.3.6.1\lib\net\Rhino.Mocks.dll"
 #load "modules.fs"
@@ -9,41 +9,26 @@
 open Rhino.Mocks
 open Rhino.Mocks.Constraints
 open System
-open FsMocks
+open FsMocks.Syntax
+
+
+#r @"System.Windows.Forms"
+open System.Windows.Forms
+
+
 
 let mock = FsMockRepository()
-let (mylist1:System.Collections.IList) = mock.strict []
-mock.record {
-    printfn "inside recorder"
-    mylist1.Add "e" |> expected AnyTimes |> constraintArgumentsTo [Is.NotNull()] |> returns 1
-    mylist1.Clear() |> expected twice
+let b:Button = mock.strict []
+let clickButtonEvent = mock.getEventRaiser (b.Click)
+
+mock.define Unordered {
+    b.Click |> subscription expected once
+    b.ResetText() |> manual_implementation (new Action(fun()-> printfn "reset text!!"))
 }
+mock.verify (fun () ->
+        b.ResetText()
+        b.Click |> Event.add (fun arg -> printfn "clicked!!")
+        clickButtonEvent.Raise(b, new EventArgs())
+)
 
 
-mock.playback {
-    let r = mylist1.Add("unknown argument")
-    printfn "r=%i" r
-    mylist1.Add("another argument") |> ignore
-    //should throw an expection : mylist1.Add(null) |> ignore
-    mylist1.Clear()
-    mylist1.Clear()
-}
-
-
-let mock2 = FsMockRepository()
-let (mylist2:System.Collections.IList) = mock2.strict []
-mock2.recordQ <@
-                    printfn "inside recorder"
-                    mylist2.Add "e" |> expected AnyTimes |> constraintArgumentsTo [Is.NotNull()] |> returns 1
-                    mylist2.Clear() |> expected twice
-@>
-
-
-mock2.playbackQ <@
-                    let r = mylist2.Add("unknown argument")
-                    printfn "inside playback r=%i" r
-                    mylist2.Add("another argument") |> ignore
-                    //should throw an expection : mylist1.Add(null) |> ignore
-                    mylist2.Clear()
-                    mylist2.Clear()
-@>
