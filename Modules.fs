9f820a40d3c916c8b90304d093b35aa0a2124911
Andri Rakotomalala <andriniaina@gmail.com>
2013-09-09 22:34:17 +0200

    hopeffully the final grammar

--- Modules.fs
+++ Modules.fs
@@ -1,4 +1,4 @@
-﻿(*
+(*
 Copyright (c) 2012, Andri Rakotomalala
  All rights reserved.
 
@@ -13,78 +13,98 @@ This software/code is distributed under the BSD license (http://opensource.org/l
 *)
 namespace FsMocks
 
-[<AutoOpen>]
+/// Sample statements :
+///     let mock = FsMockRepository()
+///     let mylist:IList = mock.strict []       // other commands : strict/reuseImplementation/autoProperties/withDefaultValues
+///     mock.define Ordered <@   /*mock statements*/   @>
+///     mock.verify <@   /*test statements*/   @>
+///
+///  example mock statements :
+///    mylist.Add 1 |> expected twice |> only_if_argument [Is.NotNull()] |> returns 1
+///    mylist.Add null |> expected once |> throws (new ArgumentException())
+///    mylist.Count |> implement_autoproperty
+///    mylist.Item(null) |> for_any_argument_value |> returns 2
+///    
 module Syntax =
     open System
     open Rhino.Mocks
     open Rhino.Mocks.Constraints
-    open Microsoft.FSharp.Quotations
-    open Microsoft.FSharp.Linq.QuotationEvaluation
 
-    type OrderOption = | Ordered | Unordered
-
-    type RepeatOption =
-        | AnyTimes
+    type RepeatOptions =
+        | AnyTime
         | Once
         | Twice
         | AtLeastOnce
         | Times of int
+    type OrderOptions = | Ordered | Unordered
 
     // lowercase synonyms of RepeatOptions
     let once = Once
-    let any_times = AnyTimes
+    let at_any_moment = AnyTime
     let twice = Twice
-    let atLeastOnce = AtLeastOnce
+    let at_least_once = AtLeastOnce
     let times = Times
-        
+    
+    type RecordBuilder() =
+        member x.Zero () = ()
+
     type FsMockRepository() =
-        let _repo = new MockRepository()
+        let repo = new MockRepository()
         /// Only the methods that were explicitly recorded are accepted as valid. This means that any call that is not expected would cause an exception and fail the test. All the expected methods must be called if the object is to pass verification.
-        member x.strict args = args |> Array.ofList |> _repo.StrictMock
+        member x.strict args = args |> Array.ofList |> repo.StrictMock
         /// Mocking only requested methods: This is available for classes only. It basically means that any non abstract method call will use the original method (no mocking) instead of relying on Rhino Mocks' expectations. You can selectively decide which methods should be mocked.
-        member x.reuseImplementation args = args |> Array.ofList |> _repo.PartialMock
+        member x.reuseImplementation args = args |> Array.ofList |> repo.PartialMock
         /// Create a dynamic mock and call PropertyBehavior on its methods
-        member x.autoProperties args = args |> Array.ofList |> _repo.Stub
+        member x.autoProperties args = args |> Array.ofList |> repo.Stub
         /// All method calls during the replay state are accepted. If there is no special handling setup for a given method, a null or zero is returned. All of the expected methods must be called for the object to pass verification.
-        member x.withDefaultValues args = args |> Array.ofList |> _repo.DynamicMock
-
-        member x.define (order:OrderOption) (expr:unit Expr) =
-            use recorder = match order with | Ordered -> _repo.Ordered() | Unordered -> _repo.Unordered()
-            expr.Eval()
-
-        member x.verify (expr:unit Expr) = 
-            _repo.ReplayAll()
-            use recorder = _repo.Playback()
-            expr.Eval()
+        member x.withDefaultValues args = args |> Array.ofList |> repo.DynamicMock
+        
+        member x.getEventRaiser (evt) =
+            //if repo.IsInReplayMode(o) then failwith "The raiser can be created only when recording"
+            evt |> Event.add (fun _ -> ())
+            let raiser = LastCall.IgnoreArguments().GetEventRaiser()
+            repo.BackToRecordAll() // reset all recorders
+            raiser
+        member x.define (order:OrderOptions) =
+            use recorder = match order with | Ordered -> repo.Ordered() | Unordered -> repo.Unordered()
+            new RecordBuilder()
+        member x.verify (f:unit->unit) = 
+            repo.ReplayAll()
+            use recorder = repo.Playback()
+            f()
 
     let ignore_arguments _ =
         LastCall.IgnoreArguments() |> ignore
-    
+    let for_any_argument_value =
+        ignore_arguments
     let ignore_property_setter =
         ignore_arguments
 
-    let only_if_argument (parameters:AbstractConstraint list) _ =
+    let only_if_argument parameters = 
         LastCall.Constraints(Array.ofList(parameters)) |> ignore
-
-    let implement_autoproperty _ =
+    let autoimplement_property _ =
         LastCall.PropertyBehavior() |> ignore
 
-    let _map_repeat (r:Interfaces.IRepeat<_>) =
-        function
-            | AnyTimes -> r.Any()
-            | Once -> r.Once()
-            | Twice -> r.Twice()
-            | AtLeastOnce -> r.AtLeastOnce()
-            | Times(i) -> r.Times(i)
-    
-    let is f = f
+    //let is f = f
 
-    let expected repeats _ =
-        let repeat = LastCall.Repeat
-        _map_repeat repeat repeats |> ignore
+    let expected occurence _ =
+        match occurence with
+            | AnyTime -> LastCall.Repeat.Any()
+            | Once -> LastCall.Repeat.Once()
+            | Twice -> LastCall.Repeat.Twice()
+            | AtLeastOnce -> LastCall.Repeat.AtLeastOnce()
+            | Times(i) -> LastCall.Repeat.Times(i)
+        |> ignore
+
+    let manual_implementation action _ =
+        LastCall.Do(action) |> ignore
+
+    /// same as expected but for events
+    let subscription expected_function occurence event =
+        event |> Event.add (fun _ -> ()) |> ignore_arguments |> expected_function occurence
     
-    let returns value _ =
+    let returns (value:obj) _ =
         LastCall.Return(value) |> ignore
-
-    let throws repeats exceptionInstance _ =
+    let throws exceptionInstance _ =
         LastCall.Throw(exceptionInstance) |> ignore
+
