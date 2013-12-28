afb8433e81631ba4e1cc4af92e613dd14b0fde2f
andri <andriniaina@gmail.com>
2013-12-28 19:58:54 +0100

    update samples and tests for multiple ref parameters

--- Tests/AnyClass.fs
+++ Tests/AnyClass.fs
@@ -4,6 +4,7 @@ type AnyInterface =
     interface
         abstract member DoSomething: unit->unit
         abstract member VirtualProperty:string with get,set
-        abstract member RefFunction:int*string byref->unit
+        abstract member RefFunction:int*string byref*int byref->unit
+        abstract member OutFunction:int->string*int
         abstract member RefFunctionWithReturnValue:int*string byref->int
     end
--- Tests/OutRef.fs
+++ Tests/OutRef.fs
@@ -7,27 +7,29 @@ open FsMocks.Syntax
 open System.Collections.Generic
 
 module OutRef =
-    let [<Fact>] ``check expected 'out' parameter``() =
+    let [<Fact>] ``check expected 'ref' parameter``() =
         use mock = new FsMockRepository()
         let o:AnyInterface = mock.strict []
         mock.define Unordered {
-            o.RefFunction(1, ref "a") |> returns_outref_params [|"b"|]
+            o.RefFunction(1, ref "a", ref 5) |> returns_outref_params [|"b"; 6|]
         }
-        let s = ref "a"
-        o.RefFunction(1, s)
-        Assert.Equal<string>("b", !s)
-    let [<Fact>] ``check unexpected 'out' parameter``() =
+        let s1 = ref "a"
+        let s2 = ref 5
+        o.RefFunction(1, s1, s2)
+        Assert.Equal<string>("b", !s1)
+        Assert.Equal(6, !s2)
+
+    let [<Fact>] ``check unexpected 'ref' parameter``() =
         Assert.Throws<Rhino.Mocks.Exceptions.ExpectationViolationException>(fun () ->
             use mock = new FsMockRepository()
             let o:AnyInterface = mock.strict []
             mock.define Unordered {
-                o.RefFunction(1, ref "a") |> returns_outref_params [|"b"|]
+                o.RefFunction(1, ref "a", ref 1) |> returns_outref_params [|"b", 2|]
             }
-            let s = ref "x"
-            o.RefFunction(1, s)
+            o.RefFunction(1, ref "wrong param", ref 1)
         )
 
-    let [<Fact>] ``check expected 'out' parameter with return value``() =
+    let [<Fact>] ``check expected 'ref' parameter with return value``() =
         use mock = new FsMockRepository()
         let o:AnyInterface = mock.strict []
         mock.define Unordered {
