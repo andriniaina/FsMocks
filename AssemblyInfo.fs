6b610ffb766c68fab2f4ccda3f5d14b8ed4d3c0c
andri <andriniaina@gmail.com>
2014-04-16 20:36:34 +0200

    assemblyinfo

---
+++ AssemblyInfo.fs
@@ -0,0 +1,25 @@
+﻿#light
+
+module AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+
+[<assembly: AssemblyTitle("FsMocks")>]
+[<assembly: AssemblyDescription("FsMocks, a mocking library for F#")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Andri Rakotomalala")>]
+[<assembly: AssemblyProduct("FsMocks")>]
+[<assembly: AssemblyCopyright("Copyright © Andri Rakotomalala, Inc. 2012")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+ 
+[<assembly: ComVisible(false)>]
+
+[<assembly: Guid("F4B59E71-51E5-4516-97B2-3A096C763D6A")>]
+
+[<assembly: AssemblyVersion("0.9.*")>]
+[<assembly: AssemblyFileVersion("0.9.0.0")>]
+()//System.DateTime.Now.ToString() |> ignore
